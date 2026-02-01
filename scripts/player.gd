5891b980ff2b8059a806d39b19c354088e84af64
alx4n <nbratek99@gmail.com>
2026-02-01 13:41:05 -0500

    Add collision shapes and work on level design

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,9 +8,41 @@ func _process(delta: float) -> void:
 	var lr_direction := Input.get_axis("ui_left", "ui_right")
 	var ud_direction := Input.get_axis("ui_up", "ui_down")
 	if lr_direction || ud_direction:
+		$idleSprite.visible = false
 		velocity.x = lr_direction * SPEED
 		velocity.y = ud_direction * SPEED
+		if velocity.x > 0.0:
+			$AnimationTree["parameters/conditions/right"] = true
+			$AnimationTree["parameters/conditions/left"] = false
+			$AnimationTree["parameters/conditions/up"] = false
+			$AnimationTree["parameters/conditions/back"] = false
+			$frontBackSprite.visible = false
+			$leftRightSprite.visible = true
+		elif velocity.x < 0.0:
+			$AnimationTree["parameters/conditions/left"] = true
+			$AnimationTree["parameters/conditions/right"] = false
+			$AnimationTree["parameters/conditions/up"] = false
+			$AnimationTree["parameters/conditions/back"] = false
+			$frontBackSprite.visible = false
+			$leftRightSprite.visible = true
+		elif velocity.y > 0.0:
+			$AnimationTree["parameters/conditions/left"] = false
+			$AnimationTree["parameters/conditions/right"] = false
+			$AnimationTree["parameters/conditions/back"] = false
+			$AnimationTree["parameters/conditions/up"] = true
+			$frontBackSprite.visible = true
+			$leftRightSprite.visible = false
+		else:
+			$AnimationTree["parameters/conditions/left"] = false
+			$AnimationTree["parameters/conditions/right"] = false
+			$AnimationTree["parameters/conditions/up"] = false
+			$AnimationTree["parameters/conditions/back"] = true
+			$frontBackSprite.visible = true
+			$leftRightSprite.visible = false
 	else:
+		$idleSprite.visible = true
+		$frontBackSprite.visible = false
+		$leftRightSprite.visible = false
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
